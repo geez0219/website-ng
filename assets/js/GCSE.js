d5a0f26f7165ac0f20d3e8ace57b100277721e4f
Purujit Bahl <purujit.bahl@ge.com>
2019-11-19 10:52:55 -0800

    Auto-generated commit

--- assets/js/GCSE.js
+++ assets/js/GCSE.js
@@ -1,13 +1,11 @@
-(function() {   
-    var cx = '007435124061301021685:nx5ivx9bz4c';
+window.onload = function(){
+    var cx = '007435124061301021685:nx5ivx9bz4c'; 
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
     gcse.src = 'https://cse.google.com/cse.js?cx='+ cx;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
-})();
 
-window.onload = function(){
     document.getElementById('gsc-i-id1').placeholder = 'Search';
 };
