fabf05ce3731aeeafe4233a04dd59fea1337d853
Vivek Soni <viveks2410@gmail.com>
2019-11-20 16:52:34 -0800

    sidebar animation finished

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; 
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router, NavigationEnd, UrlSegment } from '@angular/router';
 import { Subscription, BehaviorSubject } from 'rxjs';
 
@@ -7,6 +7,7 @@ import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 
 import { Example } from '../example';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-example',
@@ -18,15 +19,21 @@ export class ExampleComponent implements OnInit {
   selectedExample: string;
   currentSelection: string;
   currentExampleText: string;
-  
+
   segments: UrlSegment[];
 
   treeControl: NestedTreeControl<Example>;
   dataSource: MatTreeNestedDataSource<Example>;
-
+  minWidth: number = 640;
   screenWidth: number;
   private screenWidth$ = new BehaviorSubject<number>(window.innerWidth);
-  
+
+  @ViewChild('sidenav', { static: true })
+  sidenav: MatSidenav;
+
+  @ViewChild('grippy', { static: true })
+  grippy: ElementRef;
+
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.screenWidth$.next(event.target.innerWidth);
@@ -71,10 +78,10 @@ export class ExampleComponent implements OnInit {
     } else {
       this.http.get('assets/example/structure.json', {responseType: 'text'}).subscribe(data => {
         this.exampleList = <Example[]>JSON.parse(data);
-        
+
         this.dataSource.data = this.exampleList;
         this.treeControl.dataNodes = this.exampleList;
-        
+
         this.loadSelectedExample();
       });
     }
@@ -87,9 +94,9 @@ export class ExampleComponent implements OnInit {
     }
     else {
       var e: Example[] = this.flatten(this.exampleList)
-        .filter(example => 
+        .filter(example =>
           (this.segments.map(segment => segment.toString()).join('/') + ".md") === example.name);
-      
+
       if (e.length > 0) {
         this.updateExampleContent(e[0]);
         this.expandNodes(e[0].name);
@@ -102,7 +109,7 @@ export class ExampleComponent implements OnInit {
   expandNodes(exampleName: string) {
     var exampleParts: Array<string> = exampleName.split("/");
     exampleParts.pop();
-    
+
     var expandNode = this.exampleList.filter(example => example.name === exampleParts[0])[0];
     this.treeControl.expand(expandNode);
   }
@@ -112,7 +119,7 @@ export class ExampleComponent implements OnInit {
 
     this.selectedExample = example.name;
     this.currentSelection = 'assets/example/' + example.name;
-    
+
     this.getSelectedExampleText();
   }
 
@@ -122,10 +129,37 @@ export class ExampleComponent implements OnInit {
     });
   }
 
+  getImageUrl() {
+    console.log(this.sidenav.opened)
+    if (this.sidenav.opened) {
+      this.grippy.nativeElement.style.left = "20rem"
+      return "url(../../assets/images/sidebar-grippy-hide.png)"
+    } else {
+      this.grippy.nativeElement.style.left = "0rem"
+      return "url(../../assets/images/sidebar-grippy-show.png)"
+    }
+  }
+
+  checkSidebar() {
+    console.log(this.sidenav.opened)
+    if (this.sidenav.opened) {
+      this.grippy.nativeElement.style.backgroundImage = "url(../../assets/images/sidebar-grippy-hide.png)"
+      this.grippy.nativeElement.style.left = "20rem"
+    } else {
+      this.grippy.nativeElement.style.backgroundImage = "url(../../assets/images/sidebar-grippy-show.png)"
+      this.grippy.nativeElement.style.left = "0rem"
+    }
+  }
+
+  toggleMenu() {
+    this.sidenav.toggle();
+    this.checkSidebar();
+  }
+
   createRouterLink(url: string) {
     var components: Array<string> = url.substring(0, url.length - 3).split('/');
     var ret = ['/examples'];
-    
+
     return ret.concat(components);;
   }
 }
