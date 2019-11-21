d23a86ac0cb4ed0b03fee86b8054e69c2237a386
geez0219 <geez0219@gmail.com>
2019-11-20 16:31:53 -0800

    it worksserver.ts

--- server.ts
+++ server.ts
@@ -16,18 +16,20 @@
  */
 
 import 'zone.js/dist/zone-node';
-
+import * as cors from 'cors';
 import * as express from 'express';
 import {join} from 'path';
 
+
 // Express server
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
+
+app.use(cors());
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
@@ -56,3 +58,5 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
 });
+
+
