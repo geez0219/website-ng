bdf7da952feea6aade9b7816902168694b26f520
Purujit Bahl <Purujit.Bahl@ge.com>
2019-11-25 11:19:46 -0800

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,29 @@ import { FormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MarkdownModule } from 'ngx-markdown';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { GettingStartedComponent } from './getting-started/getting-started.component';
-import { TutorialComponent } from './tutorial/tutorial.component';
-import { ApiComponent } from './api/api.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatTreeModule } from '@angular/material/tree';
-import { SearchComponent } from './search/search.component';
-import { TocComponent } from './toc/toc.component';
-import { InstallComponent } from './install/install.component';
-import { CommunityComponent } from './community/community.component';
 import { ClipboardModule } from 'ngx-clipboard';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { SnackbarComponent } from './snackbar/snackbar.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ExampleComponent } from './example/example.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import { WINDOW_PROVIDERS } from './window-provider/window-provider.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { GettingStartedComponent } from './getting-started/getting-started.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { ApiComponent } from './api/api.component';
+import { TocComponent } from './toc/toc.component';
+import { InstallComponent } from './install/install.component';
+import { CommunityComponent } from './community/community.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SnackbarComponent } from './snackbar/snackbar.component';
+import { ExampleComponent } from './example/example.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +36,6 @@ import { WINDOW_PROVIDERS } from './window-provider/window-provider.component';
     GettingStartedComponent,
     TutorialComponent,
     ApiComponent,
-    SearchComponent,
     TocComponent,
     InstallComponent,
     CommunityComponent,
