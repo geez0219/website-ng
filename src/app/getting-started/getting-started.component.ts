0b8c2656f9b79643e70500ec48f41291d3414795
geez0219 <geez0219@gmail.com>
2020-05-08 15:11:58 -0700

    update for migrate to tsparticle

--- src/app/getting-started/getting-started.component.ts
+++ src/app/getting-started/getting-started.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ParticlesModule } from 'angular-particle';
-
+import { Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-getting-started',
   templateUrl: './getting-started.component.html',
@@ -18,128 +17,16 @@ export class GettingStartedComponent implements OnInit {
     name: "FastEstimator"
   }
 
-  constructor(private title: Title) { }
+  constructor( private renderer2: Renderer2,
+               private title: Title) {}
 
   ngOnInit() {
     this.title.setTitle(this.data.name);
+    const s2 = this.renderer2.createElement('script');
+    s2.type = 'text/javascript';
+    s2.src = './assets/js/tsparticles.js';
+    this.renderer2.appendChild(document.body, s2);
 
-    this.particleStyle = {
-    'position': 'absolute',
-    'width': '100%',
-    'height': '450px',
-    'background-repeat': 'no-repeat',
-    'background-color': '#23153c'
-    }
-    this.particleParams = {
-      "particles": {
-        "number": {
-          "value": 150,
-          "density": {
-            "enable": true,
-            "value_area": 650
-          }
-        },
-        "color": {
-          "value": "#0177bd"
-        },
-        "shape": {
-          "type": "polygon",
-          "stroke": {
-            "width": 0,
-            "color": "#0177bd"
-          },
-          "polygon": {
-            "nb_sides": 5
-          },
-          "image": {
-            "src": "img/github.svg",
-            "width": 100,
-            "height": 100
-          }
-        },
-        "opacity": {
-          "value": 0.5,
-          "random": false,
-          "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.1,
-            "sync": false
-          }
-        },
-        "size": {
-          "value": 3,
-          "random": true,
-          "anim": {
-            "enable": false,
-            "speed": 40,
-            "size_min": 0.1,
-            "sync": false
-          }
-        },
-        "line_linked": {
-          "enable": true,
-          "distance": 150,
-          "color": "#0177bd",
-          "opacity": 0.6887438433508188,
-          "width": 1.3
-        },
-        "move": {
-          "enable": true,
-          "speed": 6,
-          "direction": "none",
-          "random": false,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": false,
-          "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-          }
-        }
-      },
-      "interactivity": {
-        "detect_on": "window",
-        "events": {
-          "onhover": {
-            "enable": false,
-            "mode": "bubble"
-          },
-          "onclick": {
-            "enable": true,
-            "mode": "push"
-          },
-          "resize": true
-        },
-        "modes": {
-          "grab": {
-            "distance": 400,
-            "line_linked": {
-              "opacity": 1
-            }
-          },
-          "bubble": {
-            "distance": 400,
-            "size": 40,
-            "duration": 2,
-            "opacity": 8,
-            "speed": 3
-          },
-          "repulse": {
-            "distance": 200,
-            "duration": 0.4
-          },
-          "push": {
-            "particles_nb": 4
-          },
-          "remove": {
-            "particles_nb": 2
-          }
-        }
-      },
-      "retina_detect": true
-    }
+    console.log("hi");
   }
-
 }
