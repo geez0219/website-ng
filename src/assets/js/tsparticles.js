430d7c8fb7ba4a544528ac6b50831ce01078cddb
geez0219 <geez0219@yahoo.com.tw>
2020-05-16 22:55:36 -0700

    new animation setting

--- src/assets/js/tsparticles.js
+++
@@ -1,8 +0,0 @@
-tsParticles
-    .loadJSON("tsparticles", "./assets/tsparticles.json")
-    .then((container) => {
-        console.log("callback - tsparticles config loaded");
-    })
-    .catch((error) => {
-        console.error(error);
-    });
