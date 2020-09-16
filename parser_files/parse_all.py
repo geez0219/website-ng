38f8ec3f4910b304ba13853a3d4af28029a097dd
geez0219 <geez0219@yahoo.com.tw>
2020-09-16 14:42:28 -0700

    add install_parser.py

--- parser_files/parse_all.py
+++ parser_files/parse_all.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     repo_path = sys.argv[1]
     output_path = sys.argv[2]
 
-    scripts = ["fe_parser.py", "apphub_parser.py", "tutorial_parser.py"]
+    scripts = ["fe_parser.py", "apphub_parser.py", "tutorial_parser.py", "install_parser.py"]
     for script in scripts:
         script_path = os.path.abspath(os.path.join(__file__, "..", script))
         os.system("python {} {} {}".format(script_path, repo_path, output_path))
